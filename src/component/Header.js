8e83cdae5168432fcc649657af87c022c1afee2a
Aakash1220 <86371151+Aakash1220@users.noreply.github.com>
2022-07-24 21:49:20 +0530

    updated icon and header

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,14 +2,32 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import './header.scss'
 import Typography from '@mui/material/Typography';
-
+import { Button,TextField} from '@mui/material';
 const Header = () => {
   return (
     <>
-        <Box className='material-header-box'>
-     <Typography sx={{ minWidth: 100 }}>Contact</Typography>
-        <Typography sx={{ minWidth: 100 }}>Profile</Typography>
-        </Box>
+      <Box className='material-header-box'>
+
+        <img src='NewNFT.png' style={{ height: 'auto', width: '40px' }} />
+        <div className='menu-List'>
+          <Typography sx={{ minWidth: 100 ,color:'white'}}>Home</Typography>
+          <Typography sx={{ minWidth: 100 ,color:'white' }}>Explore</Typography>
+          <Typography sx={{ minWidth: 100 ,color:'white' }}>Activity</Typography>
+          <Typography sx={{ minWidth: 100  ,color:'white'}}>Contact</Typography>
+        </div>
+        <div className='menu-List menu-serachDiv'>
+        <TextField
+          
+        
+          type="search"
+          id="outlined-size-small filled-search"
+          size="small"
+         placeholder='Search'
+        />
+          <Button size="small" variant="contained" className='btn-grad'>Create</Button>
+
+        </div>
+      </Box>
     </>
   )
 }
